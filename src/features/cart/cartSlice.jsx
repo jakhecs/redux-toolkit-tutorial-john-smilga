e4401b7c1c44fe78d3bdfc34dab4ca83296402d3
jakhecs <jakhecs@gmail.com>
2023-09-07 16:31:05 +0000

    Increase, decrease and remove item

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -3,7 +3,7 @@ import cartItems from "../../cartItems";
 
 const initialState = {
   cartItems: cartItems,
-  amount: 1,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
@@ -11,8 +11,25 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, actions) => {
+      const itemId = actions.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+  },
 });
 
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
-
 // console.log(cartSlice);
